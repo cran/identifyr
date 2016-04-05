f09ec590f0b18434a958f555819670457aa91dd5
George Miranda <georgemirandajr@gmail.com>
2016-04-05 00:06:28 +0000

    version 0.1.3

--- R/clean_x.R
+++ R/clean_x.R
@@ -1,40 +1,53 @@
 
 #' @title Clean X Numbers
-#' @description Useful for correcting manually entered X-numbers that may be abbreviated or contain non-standard methods for handling null values such as "No #"
-#' @param x is a character vector containing X numbers
-#' @return Character vector of standardized X numbers. NA values are introduced where there appears to be non-X characters present.
-#' @seealso clean_pb, clean_id
+#' @description Useful for correcting manually entered or missing X-numbers that may be abbreviated or contain non-standard methods for handling null values such as "No #"
+#' @param x character vector containing X numbers
+#' @param using string indicating the name of a reference identifier to compare with. May be one of the following options: "CASE" or "CII".
+#' @param value list or character vector that contains the actual values of the known identifier.
+#' @details By default, clean_x() simply pads the string containing X numbers and ensures it meets the standard format. This is useful when correcting a small amount of X numbers, or when there is an untrustworthy reference identifier. If a clean case or CII number is available, the X numbers can be cross-referenced to a built-in table using the reference identifier. Only one reference identifier is allowed at the time of the current release.
+#' @return Character vector of standardized X numbers. By default, NA values are introduced where there appears to be non-X characters present, unless a match was found in the reference table.
+#' @seealso clean_case, clean_id, obtain_id
 #' @examples
 #' clean_x("X6789") # X00006789
 #' clean_x("No #") # NA
 #' clean_x("1234") # X00001234
+#' clean_x("X00020", using = "CASE", value = "PB021665")
+#' clean_x("X00020", using = "CII", value = "A07515123")
 
 # X-Number Cleaning
 #' @export
-clean_x <- function(x) {
+clean_x <- function(x, using = NULL, value = NULL) {
 
-  # Replace any instances where there is a space anywhere in the X number, such as "X 0000789"
+  # Strip away all characters, even if it's something that indicates an empty value such as "No PB #"
+  x <- stringr::str_replace_all(x, pattern = "[A-Za-z]", replacement = "")
+
+  # Replace all whitespace
   x <- stringr::str_replace_all(x, pattern = " ", replacement = "")
 
-  padded <- vapply(x, stringr::str_pad, FUN.VALUE = "character", width = 9, side = "left", pad = "X")  # Pad the character vector and assign to a temporary list
+  # Pad each element with zeros to become 6 digits long
+  padded <- vapply(x, stringr::str_pad, FUN.VALUE = "character", width = 8, side = "left", pad = "0")
 
-  # Replace X's with 0's when there's more than one X in an entry. Should only be X0, X00, X000, etc. Assign to new vector
-  clean.x <- stringr::str_replace_all(padded,
-                             pattern = c(
-                               "XXXXXXXXX{1}" = "X00000000",
-                               "XXXXXXXX{1}" = "X0000000",
-                               "XXXXXXX{1}" = "X000000",
-                               "XXXXXX{1}" = "X00000",
-                               "XXXXX{1}" = "X0000",
-                               "XXXX{1}" = "X000",
-                               "XXX{1}" = "X00",
-                               "^XX" = "X0")
-  )
+  # Add the PB prefix to all strings
+  padded <- stringr::str_c("X", padded)
 
-  # Find entries that have non-X numbers such as "NO X #", "Not in APS", etc. Replace these with NA values
-  clean.x[grep("[A-Wa-w]\\D|[Y-Zy-z]\\D", clean.x)] <- NA
+  # Convert the ones that originally used text to indicate null values to NA
+  padded[grep("X0000000", padded)] <- NA
 
-  # Return the cleaned up version of the character vector
-  return(clean.x)
+  ## If the using argument is not null, use the reference table to verify the X numbers.
+  if (is.null(using)) {
+    # Return the cleaned up version of the character vector
+    return(padded)
+
+  } else if (!is.null(using) & is.null(value)) {
+
+    stop("The 'value' argument is null when 'using' argument is not null")
+
+  } else {
+
+    verified <- obtain_id(obtain = "X", using = using, value = value)
+
+    return(verified)
+  }
+
 }
 
--- README.md
+++ README.md
@@ -1,33 +1,55 @@
+# Identifyr 0.1.3
+
 # Purpose 
-This package provides an efficient method for cleaning unique identifiers used by the Los Angeles County Probation Department. It contains functions that standardize identifiers and a wrapper function that applies multiple identifyr functions at once.
+This package provides efficient tools for cleaning unique identifiers used by the Los Angeles County Probation Department. It contains functions that standardize identifiers in preparation for analysis by removing extraneous characters and padding where necessary. Users also have an option to utilize an internal dataset to validate or obtain identifiers based on another identifier. A wrapper function that applies multiple identifyr functions at once is also included (similar to `dplyr::mutate`, but is maybe more user friendly for this purpose). 
     
 # How to Use It
-Users can apply the individual functions to an identifier of interest, or use the wrapper `clean_id()` to indicate the column numbers and corresponding functions to apply. The column numbers and the order of the functions must be in the same order.  The result of the wrapper function is a dataframe of the original length, but with replaced values in the indicated columns. Identifyr utilizes the magrittr pipe operator to make the code easy to read, especially when used in conjunction with `dplyr`. 
-
+Users can apply the individual functions to an identifier of interest, or use the wrapper `clean_id()` to indicate the column numbers or names and corresponding functions to apply. The user-supplied columns and the functions to apply must be in the same order (otherwise you can apply a function to the wrong column and get an error!). The result of the wrapper function is a dataframe of the original length with replaced values in the indicated columns. Identifyr utilizes the magrittr pipe operator because it is designed to be used in conjunction with other data cleaning packages that use this operator such as `dplyr` and `tidyr`. 
+  
+### Cleaning Functions
+You can apply one function at a time.
+```{r}
+clean_x("X6789")
+clean_case("PB123") 
+```
+  
+You can pass additional arguments to clean_x that verifies the identifier against a reference table.
+```{r}
+# If the case number is known, you can use this to obtain/verify the X-Number. 
+# The using argument is how you would like to verify the ID and value is the actual known ID.
+clean_x("X00020", using = "CASE", value = "PB021665")
+```
+  
+### Obtain_id
+You can obtain an identifier if you have another identifier that could be cross-referenced in the built-in table. Currently, you can obtain either X or CII numbers.
+```{r}
+obtain_id(obtain = "X",  # what you want to obtain
+  using = "CASE",        # the type of ID that you already know
+  value = "PB021665")    # the actual ID that you know
+```
+  
+### Clean_id 
+Apply the cleaning functions at once (similar to `dplyr::mutate`)
 ```{r}
 df %>%
   clean_id(
-    cols = c(1, 3),  # reference the desired columns to manipulate
-    FUN = c("clean_x", "clean_pb")  # apply these functions in this order
+    cols = c(1, 3),  # reference the desired column index or name to manipulate
+    FUN = c("clean_x", "clean_case")  # apply these functions in this order
   )
 
-  
-# Or you can apply one function at a time
-clean_x(x)
-clean_pb(x) 
-
-```
+```  
   
 # Where to Find It
-Download from github if you have the devtools package for R.
-
+The latest version of `identifyr` is available for download from github if you have the devtools package for R. The initial release is available on the Comprehensive R Archive Network (CRAN) and all future major releases will be available on CRAN.
+  
+### From Github
 ```{r}
 install.packages("devtools")
 
 devtools::install_github("georgemirandajr/identifyr")
 
 ```
-Download from Comprehensive R Archive Network (CRAN)
+### From Comprehensive R Archive Network (CRAN)
 ```{r}
 install.packages("identifyr")
 ```
